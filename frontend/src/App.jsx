e2f9b51a9d18ab08e6c8f2bf47c6e48cd04b1229
Bharath_Karthik <bharathkarthik2006@gmail.com>
2025-07-10 14:46:13 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Cart from './Pages/Cart'
+import Collection from './Pages/Collection'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Orders from './Pages/Orders'
+import PlaceOrder from './Pages/PlaceOrder'
+import Product from './Pages/Product'
+import Footer from './Components/Footer'
+import SearchBar from './Components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import Verify from './Pages/Verify'
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]'>
+      <ToastContainer/>
+      <Navbar  />
+      <SearchBar />
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/about' element= {<About/>} />
+        <Route path='/cart' element= {<Cart/>} />
+        <Route path='/collection' element= {<Collection/>} />
+        <Route path='/contact' element= {<Contact/>} />
+        <Route path='/login' element= {<Login/>} />
+        <Route path='/orders' element= {<Orders/>} />
+        <Route path='/place-order' element= {<PlaceOrder/>} />
+        <Route path='/Product/:productId' element= {<Product/>} />
+        <Route path='/verify' element={<Verify/>} />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/CartTotal.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import Title from './Title'
+
+const CartTotal = () => {
+    const {currency,deliveryfee,getCartAmount} = useContext(ShopContext)
+  return (
+    <div className='w-full'>
+        <div className='text-2xl'>
+            <Title text1={'CART'} text2={'TOTALS'} />
+        </div>
+        <div className='flex flex-col gap-2 mt-2 text-sm'>
+            <div className='flex justify-between'>
+                <p>SubTotal</p>
+                <p> {currency} {getCartAmount()}.00 </p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p>Shipping Fee</p>
+                <p>{currency} {getCartAmount()>0?deliveryfee:0}.00 </p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p>Total</p>
+                <p>{currency}{getCartAmount()===0 ? 0 : getCartAmount()+deliveryfee}.00 </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartTotal
